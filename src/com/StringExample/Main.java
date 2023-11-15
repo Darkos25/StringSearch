6a6d287655f9f63d135799996a3aa78f3ff733ea
Darkos25 <149709798+Darkos25@users.noreply.github.com>
2023-11-15 16:15:16 +0300

    Добавил новый файл

--- src/com/StringExample/Main.java
+++ src/com/StringExample/Main.java
@@ -1,8 +1,17 @@
 package com.StringExample;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+	    File SearchFile = new File("SearchFile.txt");
+        Scanner sc = new Scanner(SearchFile);
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+
     }
 }
